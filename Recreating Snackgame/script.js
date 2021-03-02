6b1b1b743b7930a185dcdd8e14fdf38be457365d
andreicampigotto <andreicampigotto@gmail.com>
2021-03-02 13:29:24 -0300

    definir fim de jogo

--- Recreating Snackgame/script.js
+++ Recreating Snackgame/script.js
@@ -27,7 +27,7 @@ function criarCobrinha(){
 }
 
 function drawFood(){
-    context.fillStyle = "yellow";
+    context.fillStyle = "blue";
     context.fillRect(food.x, food.y, box, box);
 }
 
@@ -45,6 +45,13 @@ function startGame(){
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(game);
+            alert(':( GAME  OVER ;(');
+        }
+    }
 
     criarBG();
     criarCobrinha();
